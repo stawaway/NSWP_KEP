d92cab8ef85d74b924c0a7a0af391cea2b6fa6c3
William St-Arnaud <william.starnaud@gmail.com>
2022-05-20 16:01:45 -0400

    Now we collect the stats for each scheme properly

--- collect_stats.jl
+++ collect_stats.jl
@@ -39,56 +39,76 @@ function file_data(filename)
         # get the time
         if haskey(fid, "stats/time/linear/$scheme")
             time_linear = fid["stats/time/linear/$scheme"]
-            linear_dict["time"] = time_linear
+            linear_dict["time/$scheme"] = time_linear
+        else
+            linear_dict["time/$scheme"] = -1.0
         end
 
         if haskey(fid, "stats/time/model/$scheme")
             time_model = fid["stats/time/model/$scheme"]
-            model_dict["time"] = time_model
+            model_dict["time/$scheme"] = time_model
+        else
+            model_dict["time/$scheme"] = -1.0
         end
 
         # get the distance to ideal
         if haskey(fid, "stats/ideal_distance/linear/$scheme")
             ideal_dist_linear = fid["stats/ideal_distance/linear/$scheme"]
-            linear_dict["ideal_distance"] = ideal_dist_linear 
+            linear_dict["ideal_distance/$scheme"] = ideal_dist_linear 
+        else
+            linear_dict["ideal_distance/$scheme"] = -1.0
         end
 
         if haskey(fid, "stats/ideal_distance/model/$scheme")
             ideal_dist_model = fid["stats/ideal_distance/model/$scheme"]
-            model_dict["ideal_distance"] = ideal_dist_model
+            model_dict["ideal_distance/$scheme"] = ideal_dist_model
+        else
+            model_dict["ideal_distance/$scheme"] = -1.0
         end
 
         # get the distance to nadir
         if haskey(fid, "stats/nadir_distance/linear/$scheme")
             nadir_dist_linear = fid["stats/nadir_distance/linear/$scheme"]
-            linear_dict["nadir_distance"] = nadir_dist_linear
+            linear_dict["nadir_distance/$scheme"] = nadir_dist_linear
+        else
+            linear_dict["nadir_distance/$scheme"] = -1.0
         end
 
         if haskey(fid, "stats/nadir_distance/model/$scheme")
             nadir_dist_model = fid["stats/nadir_distance/model/$scheme"]
-            model_dict["nadir_distance"] = nadir_dist_model
+            model_dict["nadir_distance/$scheme"] = nadir_dist_model
+        else
+            model_dict["nadir_distance/$scheme"] = -1.0
         end
 
         # get the POF
         if haskey(fid, "stats/pof/linear/$scheme")
             pof_linear = fid["stats/pof/linear/$scheme"]
-            linear_dict["POF"] = pof_linear
+            linear_dict["POF/$scheme"] = pof_linear
+        else
+            linear_dict["POF/$scheme"] = -1.0
         end
 
         if haskey(fid, "stats/pof/model/$scheme")
             pof_model = fid["stats/pof/model/$scheme"]
-            model_dict["POF"] = pof_model
+            model_dict["POF/$scheme"] = pof_model
+        else
+            model_dict["POF/$scheme"] = -1.0
         end
 
         # get the POU
         if haskey(fid, "stats/pou/linear/$scheme")
-            pou_linear = fid["stats/pof/linear/$scheme"]
-            linear_dict["POU"] = pou_linear
+            pou_linear = fid["stats/pou/linear/$scheme"]
+            linear_dict["POU/$scheme"] = pou_linear
+        else
+            linear_dict["POU/$scheme"] = -1.0
         end
 
         if haskey(fid, "stats/pou/model/$scheme")
-            pou_model = fid["stats/pof/model/$scheme"]
-            model_dict["POU"] = pou_model
+            pou_model = fid["stats/pou/model/$scheme"]
+            model_dict["POU/$scheme"] = pou_model
+        else
+            model_dict["POU/$scheme"] = -1.0
         end
     end
     
@@ -100,10 +120,8 @@ end
 function collect_stats(dir, output)
     # open file to store the data
     fid = h5open(output, "w") # TODO take care of this line
-    linear = Dict{String, Vector{Float64}}("time" => [], "ideal_distance" => [], "nadir_distance" => [],
-        "POF" => [], "POU" => [], "|P|" => [], "|N|" => []) 
-    model = Dict{String, Vector{Float64}}("time" => [], "ideal_distance" => [], "nadir_distance" => [], 
-        "POF" => [], "POU" => [], "|P|" => [], "|N|" => []) 
+    linear = Dict{String, Vector{Float64}}() #("time" => [], "ideal_distance" => [], "nadir_distance" => [], "POF" => [], "POU" => [], "|P|" => [], "|N|" => []) 
+    model = Dict{String, Vector{Float64}}() #("time" => [], "ideal_distance" => [], "nadir_distance" => [], "POF" => [], "POU" => [], "|P|" => [], "|N|" => []) 
 
     # loop over files and collect data
     for filename in Base.Filesystem.readdir(dir)
@@ -114,10 +132,16 @@ function collect_stats(dir, output)
         linear_dict, model_dict = file_data(joinpath(dir, filename))
 
         for (key, val) in linear_dict
+            if ! haskey(linear, key)
+                linear[key] = Vector{Float64}()
+            end
             push!(linear[key], val)
         end
 
         for (key, val) in model_dict
+            if ! haskey(model, key)
+                model[key] = Vector{Float64}()
+            end
             push!(model[key], val)
         end
     end
